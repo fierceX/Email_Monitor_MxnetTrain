6fbbf316861f080d01194e45870b1811bc7e4d09
xialuyu <fiercex@outlook.com>
2017-10-12 10:35:50 +0800

    添加文件

---
+++ EmailTool.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+
+import poplib
+import time
+from email.parser import Parser
+from email.header import decode_header
+from envelopes import Envelope
+
+
+pophost = 'pop.126.com'
+smtphost = 'smtp.126.com'
+useremail = 'trainmonitor@126.com'
+toemail = 'fiercewind@outlook.com'
+password = '123456'
+
+
+def guess_charset(msg):
+    charset = msg.get_charset()
+    if charset is None:
+        content_type = msg.get('Content-Type', '').lower()
+        pos = content_type.find('charset=')
+        if pos >= 0:
+            charset = content_type[pos + 8:].strip()
+    return charset
+
+def decode_str(s):
+    value, charset = decode_header(s)[0]
+    if charset:
+        value = value.decode(charset)
+    return value
+
+
+def Get_info(msg):
+    if (msg.is_multipart()):
+        parts = msg.get_payload()
+        for n, part in enumerate(parts):
+            return Get_info(part)
+    if not msg.is_multipart():
+        content_type = msg.get_content_type()
+        if content_type=='text/plain':
+            content = msg.get_payload(decode=True)
+            charset = guess_charset(msg)
+            if charset:
+                content = content.decode(charset)
+            return content
+
+def SentEmail(message,subject,image=True):
+    envelope = Envelope(
+    from_addr=(useremail, u'Train'),
+    to_addr=(toemail, u'FierceX'),
+    subject=subject,
+    text_body=message
+    )
+    if image:
+        envelope.add_attachment('NN.png')
+    
+    envelope.send(smtphost, login=useremail,
+              password=password, tls=True)
+    
+def ReEmail():
+    try:
+        pp = poplib.POP3(pophost)
+        pp.user(useremail)
+        pp.pass_(password)
+        resp, mails, octets = pp.list()
+        index = len(mails)
+        if index > 0:
+            resp, lines, octets = pp.retr(index)
+            msg_content = b'\r\n'.join(lines).decode('utf-8')
+            pp.dele(index)
+            pp.quit()
+            msg = Parser().parsestr(msg_content)
+            message = Get_info(msg)
+            subject = msg.get('Subject')
+            date = msg.get('Date')
+            return message,subject,date
+    except ConnectionResetError as e:
+        print('ConnectionResetError')
+    return None,None,None
+
---
+++ NN_Train.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+
+from mxnet import gluon
+from mxnet.gluon import nn
+import matplotlib.pyplot as plt
+from mxnet import autograd as autograd
+from mxnet import nd
+import mxnet as mx
+
+
+def try_gpu():
+    """If GPU is available, return mx.gpu(0); else return mx.cpu()"""
+    try:
+        ctx = mx.gpu()
+        _ = nd.zeros((1,), ctx=ctx)
+    except:
+        ctx = mx.cpu()
+    return ctx
+
+
+ctx = try_gpu()
+
+
+def transform(data, label):
+    return data.astype('float32') / 255, label.astype('float32')
+
+
+def GetDate():
+    fashion_train = gluon.data.vision.FashionMNIST(
+        root='./', train=True, transform=transform)
+    fashion_test = gluon.data.vision.FashionMNIST(
+        root='./', train=True, transform=transform)
+    return fashion_train, fashion_test
+
+
+def accuracy(output, label):
+    return nd.mean(output.argmax(axis=1) == label).asscalar()
+
+
+def evaluate_accuracy(data_iterator, net):
+    acc = 0.
+    test_loss = 0.
+    softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+    for data, label in data_iterator:
+        data = data.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+        output = net(nd.transpose(data, (0, 3, 1, 2)))
+        loss = softmax_cross_entropy(output, label)
+        acc += accuracy(output, label)
+        test_loss += nd.mean(loss).asscalar()
+    return acc / len(data_iterator), test_loss / len(data_iterator)
+
+
+def GetNN():
+    net = nn.HybridSequential()
+    with net.name_scope():
+        net.add(gluon.nn.Conv2D(channels=20, kernel_size=5, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+        net.add(gluon.nn.Conv2D(channels=50, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+        net.add(gluon.nn.Flatten())
+        net.add(gluon.nn.Dense(10))
+    net.initialize(init=mx.init.Xavier(), ctx=ctx)
+    net.hybridize()
+    return net
+
+
+def NN_Train(net, train_data, test_data, epochs, batch_size, learning_rate, weight_decay):
+    msg = ''
+    train_loss = []
+    train_acc = []
+    dataset_train = gluon.data.DataLoader(train_data, batch_size, shuffle=True)
+    test_loss = []
+    test_acc = []
+    dataset_test = gluon.data.DataLoader(test_data, batch_size, shuffle=True)
+
+    trainer = gluon.Trainer(net.collect_params(), 'adam',
+                            {'learning_rate': learning_rate,
+                             'wd': weight_decay})
+    softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+
+    for epoch in range(epochs):
+        _loss = 0.
+        _acc = 0.
+        t_acc = 0.
+        for data, label in dataset_train:
+            data = nd.transpose(data, (0, 3, 1, 2))
+            data = data.as_in_context(ctx)
+            label = label.as_in_context(ctx)
+            with autograd.record():
+                output = net(data)
+                loss = softmax_cross_entropy(output, label)
+            loss.backward()
+            trainer.step(batch_size)
+
+            _loss += nd.mean(loss).asscalar()
+            _acc += accuracy(output, label)
+        __acc = _acc / len(dataset_train)
+        __loss = _loss / len(dataset_train)
+        train_loss.append(__loss)
+        train_acc.append(__acc)
+
+        t_acc, t_loss = evaluate_accuracy(dataset_test, net)
+        test_loss.append(t_loss)
+        test_acc.append(t_acc)
+
+        msg += ("Epoch %d. Train Loss: %f, Test Loss: %f, Train Acc %f, Test Acc %f\n" % (
+            epoch, __loss, t_loss, __acc, t_acc))
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(train_loss, 'r')
+    ax1.plot(test_loss, 'g')
+    ax1.legend(['Train_Loss', 'Test_Loss'], loc=2)
+    ax1.set_ylabel('Loss')
+
+    ax2 = ax1.twinx()
+    ax2.plot(train_acc, 'b')
+    ax2.plot(test_acc, 'y')
+    ax2.legend(['Train_Acc', 'Test_Acc'], loc=1)
+    ax2.set_ylabel('Acc')
+
+    plt.savefig('NN.png', dpi=600)
+    net.collect_params().save('NN.params')
+    return msg
